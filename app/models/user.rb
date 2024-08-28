af12aaec6e7413fca32d530a6e1298264912dee8
sean-garwood <seansgarwood@gmail.com>
2024-08-28 14:46:13 -0400

    chore: user associations
    * has_many posts
    * validates with devise:
      * presence of name
      * uniqueness of email
      * length of password
      * format of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # each user has a username, email, and password
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+
+  has_many :posts, foreign_key: :author_id
 end
